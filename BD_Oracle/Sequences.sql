831cd8ff2227177343691e1999f842ca3b3a1c6c
Yoan Jouve <55242647+JouveYoan@users.noreply.github.com>
2020-11-25 14:21:27 +0100

    Update Sequences.sql

--- BD_Oracle/Sequences.sql
+++ BD_Oracle/Sequences.sql
@@ -1,48 +1,54 @@
 CREATE SEQUENCE SEQ_CR
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_DR
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_MED
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_MEDIC
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_REG
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_RS
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
+
 CREATE SEQUENCE SEQ_SEC
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
 CREATE SEQUENCE SEQ_VIS
-start with 1
-increment by 1
-maxvalue 5000
-cycle
-cache 2;
+  start with 1
+  increment by 1
+  maxvalue 5000
+  cycle
+  cache 2;
