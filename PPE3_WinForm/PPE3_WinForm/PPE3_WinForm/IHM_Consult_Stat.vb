b545590e25f59c0915d7b23482d2c9bcc47e8035
Yoan Jouve <55242647+JouveYoan@users.noreply.github.com>
2021-05-20 15:17:55 +0200

    Delete PPE3_WinForm directory

--- PPE3_WinForm/PPE3_WinForm/PPE3_WinForm/IHM_Consult_Stat.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class IHM_Consult_Stat
-
-    Private Sub Retour_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Retour.Click
-
-        Me.Hide()
-        IHM_Acceuil.Show()
-
-    End Sub
-End Class
